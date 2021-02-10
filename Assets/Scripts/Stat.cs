42238882f02555048c7ab3bf81b5d208aa90a637
emiledes2266 <emileyoo2266@outlook.fr>
2021-02-10 18:35:36 -0500

    Week 1 Upload 1.2

---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    public string characterName;
+    public int characterLevel;
+
+    public int damagePerTurn;
+
+    public int maxHp;
+    public int currentHp;
+
+    public int maxMana;
+    public int currentMana;
+}
